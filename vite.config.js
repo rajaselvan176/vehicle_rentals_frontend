cce0df9863ba837a8a12acca7f1395efbbef61b2
rajaselvan176 <rajaselvan107@gmail.com>
2025-03-15 21:32:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'https://vehicle-rentals-backend.onrender.com',
+                target: 'https://vehicle-rentals-backend.onrender.com/',
                 changeOrigin: true,
                 secure: false,
             },
